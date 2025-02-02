43ba0604c014546793af52cd550c718b13bef0b1
ilszew <kolewandowski@gmail.com>
2025-02-02 21:42:31 +0100

    for further developing we are generating random location within asked distance

--- cycleplore/TrackData.swift
+++ cycleplore/TrackData.swift
@@ -2,8 +2,37 @@ import SwiftUI
 import MapKit
 
 class TrackData: ObservableObject {
-    @Published var startLocation: MapLocation? = nil
+    @Published var startLocation: CLLocationCoordinate2D? = nil
     @Published var trackLength: Double = 20.0
     @Published var days: Int = 1
     @Published var fitnessLevel: Int = 1
+    @Published var route: [CLLocationCoordinate2D] = []
+    
+    func generateRandomDestination() {
+        guard let start = startLocation else { return }
+        
+        let earthRadius: Double = 6371.0 // km
+        let distanceRatio = trackLength / earthRadius
+        let randomBearing = Double.random(in: 0...(2 * .pi))
+        
+        let startLat = start.latitude.toRadians()
+        let startLon = start.longitude.toRadians()
+        
+        let destLat = asin(sin(startLat) * cos(distanceRatio) + cos(startLat) * sin(distanceRatio) * cos(randomBearing))
+        let destLon = startLon + atan2(sin(randomBearing) * sin(distanceRatio) * cos(startLat), cos(distanceRatio) - sin(startLat) * sin(destLat))
+        
+        let destination = CLLocationCoordinate2D(latitude: destLat.toDegrees(), longitude: destLon.toDegrees())
+        
+        route = [start, destination]
+    }
+}
+
+extension Double {
+    func toRadians() -> Double {
+        return self * .pi / 180.0
+    }
+    
+    func toDegrees() -> Double {
+        return self * 180.0 / .pi
+    }
 }
