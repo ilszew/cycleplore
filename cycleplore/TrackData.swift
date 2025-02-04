79dd3829084fab2c2819e15dbc14d9fc8ab31269
ilszew <kolewandowski@gmail.com>
2025-02-04 18:08:33 +0100

    added track to gpx conversion, add gpx download

--- cycleplore/TrackData.swift
+++ cycleplore/TrackData.swift
@@ -41,6 +41,43 @@ class TrackData: ObservableObject {
         documentPicker.allowsMultipleSelection = true
         rootViewController.present(documentPicker, animated: true, completion: nil)
     }
+    
+    func saveGPXFile() {
+        let gpxContent = generateGPX()
+        let fileName = "track.gpx"
+        let fileURL = FileManager.default.temporaryDirectory.appendingPathComponent(fileName)
+        
+        do {
+            try gpxContent.write(to: fileURL, atomically: true, encoding: .utf8)
+            shareFile(fileURL)
+        } catch {
+            print("Error saving GPX file: \(error)")
+        }
+    }
+    
+    private func generateGPX() -> String {
+        var gpx = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+        gpx += "<gpx version=\"1.1\" creator=\"Cycleplore\" xmlns=\"http://www.topografix.com/GPX/1/1\">\n"
+        gpx += "<trk>\n<trkseg>\n"
+        
+        for coord in route {
+            gpx += "<trkpt lat=\"\(coord.latitude)\" lon=\"\(coord.longitude)\"/>\n"
+        }
+        
+        gpx += "</trkseg>\n</trk>\n</gpx>"
+        return gpx
+    }
+    
+    private func shareFile(_ fileURL: URL) {
+        let activityViewController = UIActivityViewController(activityItems: [fileURL], applicationActivities: nil)
+        guard let rootViewController = UIApplication.shared.connectedScenes
+                .compactMap({ $0 as? UIWindowScene })
+                .flatMap({ $0.windows })
+                .first(where: { $0.isKeyWindow })?.rootViewController else {
+            return
+        }
+        rootViewController.present(activityViewController, animated: true, completion: nil)
+    }
 }
 
 extension Double {
